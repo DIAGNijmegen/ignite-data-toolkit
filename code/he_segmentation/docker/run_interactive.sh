40703af41a479a712748219a1c9ace6c66566636
JoeySpronck <60735928+JoeySpronck@users.noreply.github.com>
2025-08-26 16:46:32 +0200

    Update Docker image tag in run_interactive.sh

--- code/he_segmentation/docker/run_interactive.sh
+++ code/he_segmentation/docker/run_interactive.sh
@@ -10,4 +10,4 @@ docker run --rm -it \
     -v "$(pwd)":/ignite_data_toolkit \
     -w /ignite_data_toolkit/code/he_segmentation/ \
     --gpus all \
-    ignite_data_toolkit
+    ignite_data_toolkit:he
